2b088e978f55e885274b362ad11742f833dc7eb8
theang66 <anguyen1@macalester.edu>
2019-06-19 23:39:22 -0500

    Add login page

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Sign In</Text>
+      <Text>Username</Text>
+      <TextInput style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 1}} />
+      <Text>Password</Text>
+      <TextInput style={{height: 40, width: 100, borderColor: 'gray', borderWidth: 1}} />
+      <Button title="Sign In" />
     </View>
   );
 }
