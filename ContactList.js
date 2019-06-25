d3dd9e44adf61cf1858ebe8ddee4ecdfcbda4136
theang66 <anguyen1@macalester.edu>
2019-06-24 21:39:58 -0500

    Add Contact

--- ContactList.js
+++ ContactList.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
 import { Text, Button } from 'react-native';
+import Contact from './Contact';
 
 class ContactList extends Component {
   static navigationOptions = {
     title: 'Contacts',
   };
-  onPressSignIn = () => {
-    this.props.navigation.navigate('Chat');
-  }
   render() {
-    const {navigate} = this.props.navigation;
     return (
-      <Button onPress={this.onPressSignIn} title="Contact 1" />
+      <Contact />
     );
   }
 }
