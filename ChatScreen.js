a31dd0a02b284c2b6e1082d6942ea0b9f70b2062
theang66 <anguyen1@macalester.edu>
2019-07-02 12:21:18 -0500

    Update ChatScreen.js and Firebase.js

--- ChatScreen.js
+++ ChatScreen.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { Text, TextInput, View, StyleSheet } from 'react-native';
+import { GiftedChat } from 'react-native-gifted-chat';
+import Firebase from './Firebase';
 
 const offset = 24;
 const styles = StyleSheet.create({
   textbox: {
-    position: 'absolute',
-    bottom: 10,
     height: offset * 2,
     margin: offset,
     paddingHorizontal: offset,
@@ -18,15 +18,41 @@ class ChatScreen extends Component {
   static navigationOptions = ({ navigation }) => ({
     title: `${navigation.state.params.title}`
   });
+
+  // Get messages and add to current messages
+  componentDidMount() {
+    Firebase.shared.on(message =>
+      this.setState(previousState => ({
+        messages: GiftedChat.append(previousState.messages, message),
+      }))
+    );
+  }
+
+  // Unsubscribe from the database when component leaves
+  componentWillUnmount() {
+    Firebase.shared.off();
+  }
+
+  state = {
+    messages: [],
+  };
+
+  // Return name and UID for GiftedChat to parse
+  get user() {
+    return {
+      name: this.props.navigation.state.params.name,
+      _id: Firebase.shared.uid,
+    };
+  }
+
   render() {
     const {navigate} = this.props.navigation;
     return (
-      <View>
-        <TextInput
-          placeholder="Enter message"
-          style={styles.textbox}
-        />
-      </View>
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={Firebase.share.send}
+        user={this.user}
+      />
     );
   }
 }
