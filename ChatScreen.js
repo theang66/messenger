a26db33ea8c6644cada66f5a4d03544bfc19033e
theang66 <anguyen1@macalester.edu>
2019-07-02 11:58:43 -0500

    Update ChatScreen.js

--- ChatScreen.js
+++ ChatScreen.js
@@ -17,14 +17,40 @@ class ChatScreen extends Component {
   static navigationOptions = ({ navigation }) => ({
     title: `${navigation.state.params.title}`
   });
+
+  // Get messages and add to current messages
+  componentDidMount() {
+    Fire.shared.on(message =>
+      this.setState(previousState => ({
+        messages: GiftedChat.append(previousState.messages, message),
+      }))
+    );
+  }
+
+  // Unsubscribe from the database when component leaves
+  componentWillUnmount() {
+    Fire.shared.off();
+  }
+
   state = {
     messages: [],
   };
+
+  // Return name and UID for GiftedChat to parse
+  get user() {
+    return {
+      name: this.props.navigation.state.params.name,
+      _id: Fire.shared.uid,
+    };
+  }
+
   render() {
     const {navigate} = this.props.navigation;
     return (
       <GiftedChat
         messages={this.state.messages}
+        onSend={Fire.share.send}
+        user={this.user}
       />
     );
   }
